a3d01e717410ff8b13d86d5f2dc768ea6a5be581
warren jarron <98108022+warrenjarron@users.noreply.github.com>
2022-09-22 09:43:00 +0200

    first commit

---
+++ dist/main.bundle.js
@@ -0,0 +1,21 @@
+"use strict";
+(self["webpackChunkagence_web"] = self["webpackChunkagence_web"] || []).push([["main"],{
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './assets/css/style.css'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/app.js"));
+/******/ }
+]);
