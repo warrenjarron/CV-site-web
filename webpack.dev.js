1f6cb6223009171a8498cac4948259540cbb249e
warren jarron <98108022+warrenjarron@users.noreply.github.com>
2022-09-23 10:21:48 +0200

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,6 +7,6 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     static: path.resolve(__dirname, "./dist"),
-    watchFiles: ['src/pages/**/*'],
+    watchFiles: ['src//**/*'],
   },
 });
